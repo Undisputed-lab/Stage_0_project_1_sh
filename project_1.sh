d88ddec6a4f900bd798a74c7bad85d14f61a1e4b
Stephen Oyejide <oyejidestephen@gmail.com>
2025-08-31 11:30:41 +0930

    Update project_1.sh

--- project_1.sh
+++ project_1.sh
@@ -1,55 +1,61 @@
 #!/bin/bash
-# Stage 0 - Project 1: Bash Basic
+# Stage 0 Project 1 & 2
+# Author: Stephen Oyejide
+# Description: Bash script to complete Stage 0 tasks for Biocomputing and Conda installations
+
+set -euo pipefail  # safer scripting: exit on error, undefined vars, and failed pipes
+
+### ------------------ PROJECT 1: Bash Basics ------------------ ###
 
 # 1. Print your name
 echo "Stephen Oyejide"
 
 # 2. Create a folder titled your name
-mkdir -p "Stephen Oyejide"
+mkdir -p "Stephen_Oyejide"
 
-# 3. Create a new directory titled biocomputing and change to that directory
-mkdir -p biocomputing && cd biocomputing
+# 3. Create a new directory titled biocomputing and change into it
+mkdir -p biocomputing && cd biocomputing || exit
 
-# 4. Download the 3 files
-wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
-wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
-wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk -O wildtype_dup.gbk
+# 4. Download the required files
+wget -q https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+wget -q https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+wget -q https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk -O wildtype_dup.gbk
 
-# 5. Move the .fna file to the folder titled your name
-mv wildtype.fna ../"Stephen Oyejide"/
+# 5. Move the .fna file into the folder with my name
+mv wildtype.fna ../Stephen_Oyejide/
 
 # 6. Delete the duplicate gbk file
-rm wildtype_dup.gbk
+rm -f wildtype_dup.gbk
 
-# 7. Confirm if the .fna file is mutant or wild type (tatatata vs tata)
-cd ../"Stephen Oyejide"
-if grep -q "tatatata" wildtype.fna; then
-    echo "Mutant"
-    # 8. If mutant, print all matching lines into a new file
-    grep "tatatata" wildtype.fna > mutant_matches.txt
+# 7. Confirm if the .fna file is mutant or wild type
+cd ../Stephen_Oyejide || exit
+if grep -qi "tatatata" wildtype.fna; then
+    echo "The sequence is MUTANT"
+    # 8. Print all matching lines into a new file
+    grep -i "tatatata" wildtype.fna > mutant_sequences.txt
 else
-    echo "Wild type"
+    echo "The sequence is WILDTYPE"
 fi
 
-# 9. Count the number of lines (excluding the header) in the .gbk file
-cd ../biocomputing
-echo "Number of lines (excluding header) in .gbk:"
-grep -v "^LOCUS" wildtype.gbk | wc -l
+# 9. Count number of lines (excluding header) in the .gbk file
+cd ../biocomputing || exit
+grep -v "^#" wildtype.gbk | wc -l
 
-# 10. Print the sequence length of the .gbk file. (Use LOCUS tag)
-echo "Sequence length:"
-grep "^LOCUS" wildtype.gbk | awk '{print $3}'
+# 10. Print the sequence length from the LOCUS tag
+grep "LOCUS" wildtype.gbk | awk '{print $3}'
 
-# 11. Print the source organism of the .gbk file. (Use SOURCE tag)
-echo "Source organism:"
-grep "^SOURCE" wildtype.gbk | head -1 | awk '{$1=""; print $0}'
+# 11. Print the source organism from the SOURCE tag
+grep "SOURCE" wildtype.gbk | head -n 1
 
-# 12. List all the gene names of the .gbk file
-echo "Gene names:"
+# 12. List all gene names in the .gbk file
 grep "/gene=" wildtype.gbk
 
-# 13. Clear your terminal space and print all commands used today
-history > commands_used.txt
+# 13. Clear terminal space and print all commands used today
 clear
-cat commands_used.txt
 history
+
+# 14. List files in the two folders
+echo "Files in biocomputing:"
+ls ../biocomputing
+echo "Files in Stephen_Oyejide:"
+ls ../Stephen_Oyejide
